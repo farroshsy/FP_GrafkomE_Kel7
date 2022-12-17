20589a2b2208ab2fb81f0e21ecf4602b2819a61a
farroshsy <farrossyafei.205025@mhs.its.ac.id>
2022-12-17 08:59:45 +0700

    Jadii

---
+++ constant.js
@@ -0,0 +1,13 @@
+import b4 from '/textures/b4.jpg'
+import b5 from '/textures/b5.jpg'
+import b6 from '/textures/b6.jpg'
+import m1 from '/textures/m1.jpg'
+import m2 from '/textures/m1.jpg'
+import m3 from '/textures/m1.jpg'
+
+let  books = [b4,b5,b6]
+let  movies = [m1,m2,m3]
+let  projects = [b4,b5,b6]
+
+export {books,movies,projects}
+
